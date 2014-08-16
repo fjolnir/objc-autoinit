f55929532f2f352302fe1c6dfa26475b0b6806e6
Fjölnir Ásgeirsson <fjolnir@asgeirsson.is>
2014-08-16 18:12:00 +0900

    Made demo more comprehensive

--- test.m
+++ test.m
@@ -4,23 +4,42 @@
 
 @interface Klass : NSObject <FAAutoInit>
 @property(readonly) int num;
-@property(readonly) id obj;
+@property(readonly, copy) id obj;
 @property(readonly) CGRect rect;
+@property(readonly) NSString *string;
 @end
 
 @interface Klass (Initializers)
-+ (instancetype)klassWithNum:(int)aNum obj:(id)aObj rect:(CGRect)aRect;
++ (instancetype)klassWithNum:(int)aNum obj:(id)aObj rect:(CGRect)aRect string:(NSString *)aStr;
 @end
 
 @implementation Klass
+@synthesize num=_number;
+@dynamic string;
+
+- (void)awakeFromInit
+{
+    NSLog(@"Initialized with %d%@%.f%.f%.f%.f%@",
+          _number, _obj,
+          _rect.origin.x, _rect.origin.y,
+          _rect.size.width, _rect.size.height,
+          self.string);
+}
+
+static NSString *foobar;
+- (NSString *)string
+{
+    return foobar;
+}
+- (void)setString:(NSString *)aStr
+{
+    foobar = [aStr copy];
+}
 @end
 
 int main(int argc, char *argv[]) {
     @autoreleasepool {
-        Klass * const test = [Klass klassWithNum:123 obj:@456 rect:(CGRect) { {7,8},{9,10} }];
-        NSLog(@"%@: %d %@ { %.f, %.f, %.f, %.f }",
-              test, test.num, test.obj,
-              test.rect.origin.x, test.rect.origin.y,
-              test.rect.size.width, test.rect.size.height);
+        Klass * const test = [Klass klassWithNum:12 obj:@34 rect:(CGRect) { 5, 6, 7, 8 } string:@"!"];
+        NSLog(@"%@", test);
     }
 }
